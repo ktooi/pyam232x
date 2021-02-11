4e49f93a5d08a42bcc513540a36b278733ce2893
Kodai Tooi <26849015+ktooi@users.noreply.github.com>
2021-02-11 19:59:22 +0900

    Various updates.

--- am232x/am232x.py
+++ am232x/am232x.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import time
 import smbus
 from logging import getLogger
@@ -9,7 +10,44 @@ usleep = lambda x: time.sleep(x/1000000.0)
 
 
 class AM232x(object):
+    """ AM2321/AM2322 に対応する Python モジュールです。
 
+    センサーから気温及び湿度を取得します。また、取得した気温及び湿度から不快指数を計算することもできます。
+
+    Examples:
+
+        次のようにシンプルに使うことができます。
+
+        Simple usage is as follows.
+
+        >>> from am232x import AM232x
+        >>>
+        >>> am232x = AM232x(name="am2322")
+        >>>
+        >>> am232x.humidity
+        >>> am232x.temperature
+        >>> am232x.discomfort
+
+        次のように、センサーとの通信のタイミングを細かく制御することもできます。
+
+        The timing of communication with the sensor can also be finely controlled, as shown below. 
+
+        >>> from am232x import AM232x
+        >>>
+        >>> am232x = AM232x(name="am2322", wakeup=False)
+        >>>
+        >>> am232x.wakeup()
+        >>> am232x.set_write_mode()
+        >>> am232x.measure()
+        >>> am232x.read()
+        >>>
+        >>> am232x.humidity
+        >>> am232x.temperature
+        >>> am232x.discomfort
+    """
+
+    # AM2321/AM2322 の I2C アドレス。
+    # I2C address of AM2321/AM2322.
     chip_addr = 0x5c
 
     # define wait micro sec.
@@ -24,40 +62,59 @@ class AM232x(object):
         self._bus = bus
         self._retry_wait = retry_wait
         self._retry_num = retry_num
+        self._wakeup = False
         self._write_mode = False
         self._measured = False
+        self._read_time = 0
         if wakeup:
             self.wakeup()
 
-    def _write_retry(self, func, register, data):
+    def _func_i2c_retry(self, func, args, retry_wait=None, retry_num=None):
         chip_addr = self.chip_addr
+        if retry_wait is None:
+            retry_wait = self._retry_wait
+        if retry_num is None:
+            retry_num = self._retry_num
         cnt = 0
         while True:
             try:
-                func(chip_addr, register, data)
-                break
+                return func(chip_addr, *args)
             except IOError as e:
-                if cnt < self._retry_num:
-                    usleep(self._retry_wait)
+                if cnt < retry_num:
+                    usleep(retry_wait)
                     cnt += 1
+                    logger.debug(("{name} : Execute the \"{func}\" was failed. retry count: {cnt}/{limit}: Exception: {exception}"
+                                  .format(name=self._name, func=func.__name__, cnt=cnt, limit=retry_num, exception=e)))
                 else:
                     raise e
 
     def _write_byte_data(self, register, data):
         i2c = self._i2c
-        self._write_retry(i2c.write_byte_data, register, data)
+        args = (register, data)
+        self._func_i2c_retry(func=i2c.write_byte_data, args=args)
 
     def _write_i2c_block_data(self, register, data_list):
         i2c = self._i2c
-        self._write_retry(i2c.write_i2c_block_data, register, data_list)
+        args = (register, data_list)
+        self._func_i2c_retry(func=i2c.write_i2c_block_data, args=args)
 
     def _read_i2c_block_data(self, register, length):
         i2c = self._i2c
-        chip_addr = self.chip_addr
-        return i2c.read_i2c_block_data(chip_addr, register, length)
+        args = (register, length)
+        return self._func_i2c_retry(func=i2c.read_i2c_block_data, args=args, retry_wait=200000)
 
     def wakeup(self):
-        self._write_byte_data(0x00, 0x00)
+        if self._wakeup:
+            return
+        i2c = self._i2c
+        chip_addr = self.chip_addr
+        cur_time = time.time()
+        
+        try:
+            i2c.write_byte_data(chip_addr, 0x00, 0x00)
+        except:
+            pass  # wakeup は必ず通信が失敗する。これは AM2321/2322 の仕様。
+        self._wakeup = True
         usleep(self.wait_wakeup)
 
     def set_write_mode(self):
@@ -66,21 +123,25 @@ class AM232x(object):
         usleep(self.wait_writemode)
 
     def measure(self):
+        self.wakeup()
         if not self._write_mode:
             self.set_write_mode()
         self._write_i2c_block_data(0x03, [0x00, 0x04])
         self._measured = True
         usleep(self.wait_readmode)
+        if hasattr(self, "_raw_data"):
+            # "_raw_data" を削除し、 self._calc() 実行時に再度 self.read() が実行されるようにする。
+            delattr(self, "_raw_data")
         self._del_properties()
 
     def check_err(self):
-        raw = self._raw
+        raw = self._raw_data
         code = raw[2]
         if code >= 0x80:
             raise ReceiveAM232xDataError(error_code=code, chip_name=self._name)
 
     def check_crc(self):
-        raw = self._raw
+        raw = self._raw_data
         rcv_crcsum = raw[7] << 8 | raw[6]
         clc_crcsum = 0xffff
 
@@ -92,23 +153,43 @@ class AM232x(object):
                     clc_crcsum ^= 0xa001
                 else:
                     clc_crcsum = clc_crcsum >> 1
-    
+
         if rcv_crcsum != clc_crcsum:
             raise AM232xCrcCheckError(recv_crc=rcv_crcsum, calc_crc=clc_crcsum, chip_name=self._name)
 
-    def read(self, check_err=True, check_crc=True):
-        if not self._measured:
-            self.measure()
-        self._raw = self._read_i2c_block_data(0x00, 8)
-        if check_err:
-            self.check_err()
-        if check_crc:
-            self.check_crc()
+    def read(self, check_err=True, check_crc=True, retry_num=10, retry_wait=2000000):
+        cnt = 0
+        while True:
+            if not self._measured:
+                self.measure()
+            if not hasattr(self, "_raw_data"):
+                self._raw_data = self._read_i2c_block_data(0x00, 8)
+                self._del_properties()
+                self._wakeup = False
+                self._write_mode = False
+                self._read_time = time.time()
+                try:
+                    if check_err:
+                        self.check_err()
+                    if check_crc:
+                        self.check_crc()
+                except AM232xError as e:
+                    if cnt < retry_num:
+                        self._measured = False
+                        cnt += 1
+                        logger.debug(("{name} : AM232x error was occurred. retry count: {cnt}/{limit}, Exception: {exception}"
+                                      .format(name=self._name, cnt=cnt, limit=retry_num, exception=e)))
+                        usleep(retry_wait)
+                        continue
+                    else:
+                        raise e
+
+            return self._raw_data
 
     def _calc(self, high_idx, low_idx):
-        if not hasattr(self, "_raw"):
+        if not hasattr(self, "_raw_data"):
             self.read()
-        raw = self._raw
+        raw = self._raw_data
         return (raw[high_idx] << 8 | raw[low_idx]) / 10.0
 
     def _del_properties(self):
